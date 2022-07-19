dc4a50d0c490fe7fbffaeefa57a3ebe94e933155
AzamatXakimov <typetiger59@gmail.com>
2022-07-19 17:19:16 +0500

    Add overflow hidden in body

--- js/main.js
+++ js/main.js
@@ -2,17 +2,21 @@ var elOpenModalBtn = document.querySelector(".js-open-modal");
 var elModal = document.querySelector(".modal");
 var elCloseModalBtn = document.querySelector(".js-close-modal")
 var elXCloseModalBtn = document.querySelector(".js-x-close-modal")
+var elBodyModal = document.querySelector("body")
 
 elOpenModalBtn.addEventListener("click", function(){
     elModal.classList.add("modal-open");
+    elBodyModal.classList.add("stop-body");
 });
 
 elCloseModalBtn.addEventListener("click", function(){
     elModal.classList.remove("modal-open");
+    elBodyModal.classList.remove("stop-body");
 });
 
 elXCloseModalBtn.addEventListener("click", function(){
-    elModal.classList.remove("modal-open")
+    elModal.classList.remove("modal-open");
+    elBodyModal.classList.remove("stop-body");
 })
 
 // elModal.addEventListener("click", function(){
